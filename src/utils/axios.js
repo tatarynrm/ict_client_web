51244fed39182e91185d6f6201305e21601761a0
tatarynrm <tatarynrm@gmail.com>
2023-06-19 09:55:28 +0300

    comment

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -4,6 +4,7 @@ const instance = axios.create({
   // baseURL: "http://192.168.1.33:8800",
   baseURL: "http://192.168.5.180:8800",
 });
+// this is comment
 instance.interceptors.request.use((config) => {
   config.headers.Authorization = window.localStorage.getItem("token");
   return config;
