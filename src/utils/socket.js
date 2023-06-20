f8e3603360c4e1ae9aa79c0aac299b1e5b02e8a1
tatarynrm <tatarynrm@gmail.com>
2023-06-20 12:14:14 +0300

    all done

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,7 +1,6 @@
 import io from "socket.io-client";
 
-// const socket = io("http://192.168.5.180:8800");
-const socket = io("http://localhost:8800");
-// const socket = io("http://192.168.2.124:5002");
+const socket = io("http://192.168.5.180:8800");
+// const socket = io("http://localhost:8800");
 
 export default socket;
